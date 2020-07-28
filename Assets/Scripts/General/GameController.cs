48d7069d1dc9d72ce7c8a09c9aadcff758a6aac3
tuananh940205 <tuan.anh.94.02.05@gmail.com>
2020-07-28 08:37:14 +0700

    Initial commit

---
+++ Assets/Scripts/General/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Systems _systems;
+
+    void Start()
+    {
+        Contexts contexts = Contexts.sharedInstance;
+        _systems.Initialize();
+    }
+
+    void Update()
+    {
+        _systems.Execute();
+        _systems.Cleanup();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Match-Three-MadeWithEntitas-02
+ 
